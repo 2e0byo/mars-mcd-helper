da40ea9437b0608d5a2f713d06ba1242bab04bd0
John Maximilian <2e0byo@gmail.com>
2021-08-16 13:08:31 +0200

    helper fn

--- src/mars_mcd_helper/__init__.py
+++ src/mars_mcd_helper/__init__.py
@@ -5,5 +5,12 @@ Utilities for retrieving and processing data from the Mars Climate Database
 """
 
 from typing import List
+from .read_mars_data import *
+from .get_mars_data import *
 
 __all__: List[str] = []  # noqa: WPS410 (the only __variable__ we use)
+
+
+def get_parse_data(**kwargs):
+    dataf = fetch_data(**kwargs)
+    return read_ascii_data(dataf)
