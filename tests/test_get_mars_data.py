89dc782af7622b88f577aece5fceb0a2ce0425a0
John Maximilian <2e0byo@gmail.com>
2021-08-16 13:31:59 +0200

    smoketest fetch data

--- tests/test_get_mars_data.py
+++ tests/test_get_mars_data.py
@@ -1,5 +1,10 @@
-from mars_mcd_helper.get_mars_data import generate_fn
+from mars_mcd_helper.get_mars_data import generate_fn, fetch_data
 
 
 def test_generate_fn():
     assert generate_fn(var1="this", var2="that", num=7) == "marsdata_this-that-7.txt"
+
+
+def test_fetch_data(tmp_path):
+    fn = fetch_data(outdir=tmp_path, ls=87.3)
+    assert fn.exists()
