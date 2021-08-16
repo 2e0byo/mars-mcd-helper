c2223b5b8f0c04777c930ef1c25a27c250ff236a
John Maximilian <2e0byo@gmail.com>
2021-08-16 13:07:01 +0200

    test for int

--- tests/test_get_mars_data.py
+++ tests/test_get_mars_data.py
@@ -2,4 +2,4 @@ from mars_mcd_helper.get_mars_data import generate_fn
 
 
 def test_generate_fn():
-    assert generate_fn(var1="this", var2="that") == "marsdata_this-that.txt"
+    assert generate_fn(var1="this", var2="that", num=7) == "marsdata_this-that-7.txt"
