312fa48dfeb5a352baa353b9267d56643f81b17a
John Maximilian <2e0byo@gmail.com>
2021-08-17 11:11:12 +0200

    documentation

--- src/mars_mcd_helper/get_mars_data.py
+++ src/mars_mcd_helper/get_mars_data.py
@@ -1,4 +1,13 @@
-"""Get data from the MCD by scraping the cgi interface."""
+"""This module handles getting data from the MCD by scraping the cgi interface.
+ We simply pass parameters up in the url, like the web version interface does.
+Then we scrape the resulting web page for the link to the data and (optionally)
+the image[s].
+
+
+Note that this is a simple scraper and is not in any sense affiliated with the
+MCD project.  Please do not run it against the server too often or
+unreasonably.  Where possible use the saved output (this is why we provide a
+saved output)."""
 from logging import getLogger
 from pathlib import Path
 from typing import Union
@@ -48,6 +57,10 @@ base_params = {
     "latpoint": None,
     "lonpoint": None,
 }
+"""Parameters which can be passed to the server.  Defaults set here are
+extracted from the web interface.  Any parameter set to `None` will not be
+passed.  To pass `"none"` use a string.  Do not override this dict directly;
+rather pass the parameter and value as keyword arguments to `fetch_data()`."""
 
 
 urlbase = "http://www-mars.lmd.jussieu.fr/mcd_python/"
@@ -55,7 +68,9 @@ url = urlbase + "cgi-bin/mcdcgi.py"
 
 
 def generate_fn(**params) -> str:
-    """Generate a unique filename from given params.
+    """Generate a unique filename from given params.  This function is used
+    internally with the parameters used by `fetch_data()`.  It is provided here
+    in case you need to generate the filename from a given set of params.
 
     Args:
         **params: params to consider.
@@ -63,16 +78,18 @@ def generate_fn(**params) -> str:
     Returns:
         (str): Fn from params.
     """
-    fn = "-".join(str(x) for _, x in params.items() if x)
+    fn = "-".join(f"{k}_{x}" for k, x in params.items() if x)
     return f"marsdata_{fn}.txt"
 
 
 class FetchingError(Exception):
-    """Error fetching resource."""
+    """Error fetching resource.  The server returns `200` with an html error
+    message, so we raise an exception and pass the error message up."""
 
 
 def fetch_data(outdir: Union[Path, str] = ".", **params):
-    """Fetch data from the MCD and save in outdir.
+    """Fetch data from the MCD and save in outdir.  Keyword arguments (other
+    than `outdir`) will override the defaults in `base_params`.
 
     Args:
         outdir (Union[Path, str]): dir to save in (Default value = ".")
@@ -83,6 +100,16 @@ def fetch_data(outdir: Union[Path, str] = ".", **params):
 
     Returns:
         (Path): output file.
+
+    Call this function to retrieve data from the server and save it in a file.
+    Keyword arguments passed here will override the defaults in `base_params`,
+    e.g.:
+
+    ```python
+    >> fetch_data(ls=0.5, localtime=1)
+    Path("marsdata_ls_0.5-localtime_1.txt")
+    ```
+    For more information on any particular parameter see the web interface.
     """
     p = base_params.copy()
     p.update(params)
