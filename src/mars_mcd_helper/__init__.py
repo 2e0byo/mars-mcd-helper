653d27539747735a7b3b933d9a93b41795e1be8a
John Maximilian <2e0byo@gmail.com>
2021-08-17 11:15:05 +0200

    caching fn

--- src/mars_mcd_helper/__init__.py
+++ src/mars_mcd_helper/__init__.py
@@ -6,7 +6,7 @@ Utilities for retrieving and processing data from the Mars Climate Database
 
 from typing import List
 
-from .get_mars_data import fetch_data
+from .get_mars_data import fetch_data, generate_fn
 from .read_mars_data import read_ascii_data
 
 __all__: List[str] = []  # noqa: WPS410 (the only __variable__ we use)
@@ -14,7 +14,8 @@ __all__: List[str] = []  # noqa: WPS410 (the only __variable__ we use)
 
 def get_parse_data(**kwargs) -> dict:
     """
-    Get and parse data.  This is a convenience function.
+    Get and parse data.  This is a convenience function.  It first checks for
+    already downloaded data and uses that in preference.
 
     Args:
         **kwargs: to pass to fetch.
@@ -23,5 +24,11 @@ def get_parse_data(**kwargs) -> dict:
         (dict): The data.
 
     """
-    dataf = fetch_data(**kwargs)
+    if "outdir" not in kwargs:
+        kwargs["outdir"] = Path(".")
+    elif isinstance(kwargs["outdir"], str):
+        kwargs["outdir"] = Path(kwargs["outdir"])
+    dataf = kwargs["outdir"] / generate_fn({k:v for k:v in params if k != "outdir"})
+    if not dataf.exists():
+        dataf = fetch_data(**kwargs)
     return read_ascii_data(dataf)
